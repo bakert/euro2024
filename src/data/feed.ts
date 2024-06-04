ca96344ee622be77e28f5150030f34ba23253dba
Thomas David Baker <bakert@gmail.com>
2024-06-03 22:04:36 -0700

    Call the main data item feed not matches

--- src/data/feed.ts
+++ src/data/feed.ts
@@ -7,7 +7,7 @@ import type { FeedT, MatchT } from "../models/models.ts";
 const response = await fetch("https://bluebones.net/matches.json");
 const matches = await response.json();
 
-const data: FeedT = {
+const feed: FeedT = {
     matches: [],
     lastMatches: [],
     currentMatches: [],
@@ -24,18 +24,17 @@ matches.forEach((match: MatchT) => {
         home: home,
         away: away,
     };
-    data.matches.push(m);
+    feed.matches.push(m);
 });
 
-const upcomingMatches = data.matches.filter((m: MatchT) => m.status === Status.UPCOMING);
+const upcomingMatches = feed.matches.filter((m: MatchT) => m.status === Status.UPCOMING);
 const minKickoffDate = Math.min(...upcomingMatches.map(m => m.kickoff.getTime()));
-data.nextMatches = upcomingMatches.filter((m: MatchT) => m.kickoff.getTime() === minKickoffDate);
+feed.nextMatches = upcomingMatches.filter((m: MatchT) => m.kickoff.getTime() === minKickoffDate);
 
-const finishedMatches = data.matches.filter((m: MatchT) => m.status === Status.FINISHED);
+const finishedMatches = feed.matches.filter((m: MatchT) => m.status === Status.FINISHED);
 const maxKickoffDate = Math.max(...finishedMatches.map(m => m.kickoff));
-data.lastMatches = finishedMatches.filter((m: MatchT) => m.kickoff === maxKickoffDate);
+feed.lastMatches = finishedMatches.filter((m: MatchT) => m.kickoff === maxKickoffDate);
 
-data.currentMatches = data.matches.filter((m: MatchT) => m.status === Status.CURRENT)
+feed.currentMatches = feed.matches.filter((m: MatchT) => m.status === Status.CURRENT)
 
-// BAKERT the word matches is working overtime here. better names
-export default data;
+export default feed;
