321ab9493758888deb8fea00952621f1b6e11524
Thomas David Baker <bakert@gmail.com>
2024-06-04 21:07:44 -0700

    Add missing script

---
+++ src/scripts/time.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    dayjs.extend(window.dayjs_plugin_advancedFormat);
+    dayjs.extend(window.dayjs_plugin_relativeTime);
+    document.querySelectorAll("time").forEach((t) => {
+        const datetime = t.getAttribute("datetime");
+        const format = t.dataset.format;
+        if (!datetime) {
+            return;
+        }
+        if (format === "fromNow") {
+            t.textContent = dayjs(datetime).fromNow();
+        } else {
+            t.textContent = dayjs(datetime).format(format);
+        }
+    });
+});
